e776f5f8d256ae078dea3913f5d87b57f2b4a0e7
lilneuromancer <neuromancer.kitten@gmail.com>
2021-09-20 16:34:08 -0500

    added wrapped erc1155 and initializer

--- contracts/SoundERC1155.sol
+++ contracts/SoundERC1155.sol
@@ -1,22 +1,50 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.4;
 
-import "hardhat/console.sol";
+import {WrappedERC1155Token} from "./WrappedERC1155Token.sol";
 
-contract Greeter {
-    string private greeting;
-
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+/**
+    @notice An ERC-1155 multitoken where each ID is wrapped in an ERC-20 interface
+ */
+contract SoundERC1155 is WrappedERC1155Token {
+    
+   function __LOUD1155_init(
+        address admin,
+        string calldata uri,
+        address _wrapperTemplate,
+        bool _deployWrapperOnMint,
+        string memory _baseName,
+        string memory _baseSymbol,
+        uint8 _decimals
+    ) internal initializer {
+        __ERC1155Base_init(admin, uri);
+        __WrappedERC1155Token_init_unchained(
+            _wrapperTemplate,
+            _deployWrapperOnMint,
+            _baseName,
+            _baseSymbol,
+            _decimals
+        );
     }
-
-    function greet() public view returns (string memory) {
-        return greeting;
+    
+    function initialize(
+        address admin,
+        string calldata uri,
+        address _wrapperTemplate,
+        bool _deployWrapperOnMint,
+        string memory _baseName,
+        string memory _baseSymbol,
+        uint8 _decimals
+    ) external virtual initializer {
+        __LOUD1155_init(
+            admin,
+            uri,
+            _wrapperTemplate,
+            _deployWrapperOnMint,
+            _baseName,
+            _baseSymbol,
+            _decimals
+        );
     }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
-    }
+    
 }
